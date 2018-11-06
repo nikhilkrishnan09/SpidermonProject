e934d489e75135a7e2817be3edec85cf42a4d41c
achintalapati <achintalapati@wpi.edu>
2018-11-06 08:12:50 -0500

    comment

--- src/spidermon/Launcher.java
+++ src/spidermon/Launcher.java
@@ -8,7 +8,7 @@ public class Launcher {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		
-		
+		//meaningful comment
 		config.height = 600;
 		config.width = 900;
 		config.vSyncEnabled = true;
